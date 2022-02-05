5be5fdbb3dd23bfb016598cc5006122f98b92cd1
josinaldojr <josinaldo.pontes@dcx.ufpb.br>
2022-02-05 00:06:34 -0300

    feat: create services to update and delete product

--- src/modules/products/services/DeleteProductService.ts
+++ src/modules/products/services/DeleteProductService.ts
@@ -1,14 +1,13 @@
 import { AppError } from '@shared/errors/AppError';
 import { getCustomRepository } from 'typeorm';
-import { Product } from '../typeorm/entities/Product';
 import { ProductRepository } from '../typeorm/repositories/ProductsRepository';
 
 interface IRequest {
   id: string;
 }
 
-class ShowProductService {
-  public async execute({ id }: IRequest): Promise<Product> {
+class DeleteProductService {
+  public async execute({ id }: IRequest): Promise<void> {
     const productRepository = getCustomRepository(ProductRepository);
 
     const product = await productRepository.findOne(id);
@@ -17,8 +16,8 @@ class ShowProductService {
       throw new AppError('Product not found');
     }
 
-    return product;
+    await productRepository.remove(product);
   }
 }
 
-export { ShowProductService };
+export { DeleteProductService };
