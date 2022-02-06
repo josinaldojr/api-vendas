86dcd23a32884725d5b730f5a1932eafae752a7f
josinaldojr <josinaldo.pontes@dcx.ufpb.br>
2022-02-06 12:14:04 -0300

    feat: create overriding types to request in express

--- src/shared/http/middlewares/isAuthenticated.ts
+++ src/shared/http/middlewares/isAuthenticated.ts
@@ -3,6 +3,12 @@ import { AppError } from '@shared/errors/AppError';
 import { NextFunction, Request, Response } from 'express';
 import { verify } from 'jsonwebtoken';
 
+interface TokenPayload {
+  iat: number;
+  exp: number;
+  sub: string;
+}
+
 function isAuthenticated(
   request: Request,
   response: Response,
@@ -17,7 +23,13 @@ function isAuthenticated(
   const [, token] = authHeader.split(' ');
 
   try {
-    const decodeToken = verify(token, authConfig.secret);
+    const decodedToken = verify(token, authConfig.secret);
+
+    const { sub } = decodedToken as TokenPayload;
+
+    request.user = {
+      id: sub,
+    };
 
     return next();
   } catch {
